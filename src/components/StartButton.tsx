72e0e00ec733665e55efd4d8f45452ac3afe764a
soudegesu <soudegesu@hotmail.co.jp>
2020-12-18 14:44:06 +0900

    put start button

---
+++ src/components/StartButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@material-ui/core';
+import React, { FC } from 'react';
+
+const StartButton: FC = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Start
+    </Button>
+  );
+};
+
+export default StartButton;
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,7 +1,17 @@
+import { Box, Grid, GridList } from '@material-ui/core';
 import React, { FC } from 'react';
+import StartButton from '../components/StartButton';
 
 const TopPage: FC = () => {
-  return <></>;
+  return (
+    <Box paddingTop={10}>
+      <GridList>
+        <Grid>
+          <StartButton />
+        </Grid>
+      </GridList>
+    </Box>
+  );
 };
 
 export default TopPage;
